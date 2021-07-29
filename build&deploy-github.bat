8bd1275b87dd9980e78239564e535699c15f2a6e
Subu89 <sudipmhnt@gmail.com>
2021-07-29 14:12:55 +0530

    penultimate commit

--- build&deploy-github.bat
+++ build&deploy-github.bat
@@ -1,3 +1,3 @@
-call ng build --prod --base-href "https://lenzoburger.github.io/MyPortfolio/"
-call npx ngh --dir=dist/MyPortfolio --message="Auto commit & deploy"
+call ng build --prod --base-href "https://subu89.github.io/my-portfolio/"
+call npx ngh --dir=dist/my-portfolio --message="Auto commit & deploy"
 call echo Successfully built and deployed!
